a6041d034d6037311d7abcc366c345414581f7b7
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-07-15 23:08:06 -0500

    .

---
+++ Router.php
@@ -0,0 +1,73 @@
+<?php
+namespace MVC;
+
+class Router {
+    public $rutasGET = [];
+    public $rutasPOST = [];
+
+    public function get($url, $fn) {
+        $this->rutasGET[$url] = $fn;
+    }
+    public function post($url, $fn) {
+        $this->rutasPOST[$url] = $fn;
+    }
+
+    public function comprobarRutas() {
+        // session_start();
+        // $auth = $_SESSION['login'] ?? null;
+        // debuguear($auth);
+      
+
+        // arreglo de rutas protegidas
+        // $rutas_protegidas= ['/admin', '/admin/propiedades/crear', '/admin/propiedades/actualizar', '/admin/propiedades/eliminar', '/admin/vendedores/crear', '/admin/vendedores/actualizar', '/admin/vendedores/eliminar'];
+
+        $urlActual = $_SERVER['REQUEST_URI'] ?? '/';
+        $urlActual = str_replace('/Appsalon', '', $urlActual);
+        $urlActual = strtok($urlActual, '?');
+        $metodo= $_SERVER['REQUEST_METHOD'];
+      
+
+        if($metodo === 'GET') {
+            $fn = $this->rutasGET[$urlActual] ?? null;
+        } else {
+            // debuguear($_POST);
+            $fn = $this->rutasPOST[$urlActual] ?? null;
+        }
+
+        // // Proteger las rutas inarray me devuelve true si la url actual esta en el arreglo de rutas protegidas
+        // if(in_array($urlActual,$rutas_protegidas) && !$auth){
+           
+        //     header('Location:/appsalon/login');
+
+        // }
+ 
+          if($fn) {
+              
+                // si la ruta existe, llamar al callback con la instancia del router
+              call_user_func($fn, $this);
+                
+          } else {
+               echo 'Página no encontrada';
+          }
+       
+        
+     } 
+        //  Muestra una vista
+     public function render($view,$datos=[]) {
+
+        // acceder a la llave valor del arreglo
+        foreach($datos as $key => $value){
+            // $$ variable de variable
+            $$key = $value;
+
+        }
+
+        ob_start(); //inicia el almacenamiento en el memoria
+         include __DIR__ . "/views/$view.php";
+         $contenido = ob_get_clean(); //limpia la memoria y lo guarda en la variable
+         include __DIR__ . "/views/template/layout.php";
+
+     }
+}
+?>
+
---
+++ controllers/LoginController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Controllers;
+use MVC\Router;
+ 
+
+class LoginController
+{
+    public static function login(Router $router)
+    {
+        echo "Desde el Controlador login";
+    }
+    public static function logout(Router $router)
+    {
+        echo "Desde el Controlador logout";
+    }
+    public static function olvide(Router $router)
+    {
+        echo "Desde el Controlador olvide contraseña";
+    }
+    public static function recuperar(Router $router)
+    {
+        echo "Desde el Controlador recuperar contraseña";
+    }
+}
