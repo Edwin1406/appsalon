6832a1f0b1a225c3789d8c763ffb2c8068237cc6
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-07-16 11:56:16 -0500

    Update app.php

--- includes/app.php
+++ includes/app.php
@@ -2,8 +2,8 @@
 
 require 'funciones.php';
 require 'database.php';
-require_once __DIR__ . '/autoload.php';
-
+require_once __DIR__ . '/../vendor/autoload.php';
+echo __DIR__;
 
 // Conectarnos a la base de datos
 use Model\ActiveRecord;
