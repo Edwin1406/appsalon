6ca42b9453965c56fbc66eb79e3a385fc0e2c124
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-07-16 15:31:09 -0500

    .

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,11 +1,2 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<h1>login</h1>
-</body>
-</html>
+<h1 class="nombre-pagina">Login</h1>
+<p class="descripcion-pagina">Inicia Sesión con tus datos</p>
