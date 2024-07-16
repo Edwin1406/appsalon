bb05110b1590f5a42e1661b1a91ca20dc4ac5825
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-07-16 18:17:51 -0500

    Update alertas.php

--- views/templates/alertas.php
+++ views/templates/alertas.php
@@ -1,5 +1,12 @@
 <?php
 
-debuguear($alertas);
+foreach( $alertas as $key => $alerta){
+    foreach($alerta as $mensaje){
+        echo "<div class='alerta $key'>";
+        echo $mensaje;
+        echo "</div>";
+    }
+
+}
 
 ?>
