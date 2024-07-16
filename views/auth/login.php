fc71a6613189537e75c871dc7b4d64a27103e51d
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-07-16 15:35:46 -0500

    .

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,2 +1,14 @@
 <h1 class="nombre-pagina">Login</h1>
 <p class="descripcion-pagina">Inicia Sesión con tus datos</p>
+
+<form class="formulario" method="POST" action="/">
+    <div class="campo">
+        <label for="email">Email</label>
+        <input type="email" id="email" placeholder="Tu Email" name="email">
+    </div>
+    <div class="campo">
+        <label for="password">Password</label>
+        <input type="password" id="password" placeholder="Tu Password" name="password">
+    </div>
+    <input type="submit" class="boton boton-verde" value="Iniciar Sesión">
+</form>
