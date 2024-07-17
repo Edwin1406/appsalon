2317ac893d1879de3fc995a132d1068777ecf150
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-07-17 00:14:58 -0500

    Update test.php

--- test.php
+++ test.php
@@ -1,8 +1,7 @@
 <?php
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use Classes\Email;
 
 $email = new Email('test@example.com', 'John', 'Doe', 'token123');
 echo "Email class loaded successfully.";
-sdsfdsfdsfdsfdsf
