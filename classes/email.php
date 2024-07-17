89b5527ea79a003686ef9d66654273059fe4052c
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-07-17 00:19:08 -0500

    Update email.php

--- classes/email.php
+++ classes/email.php
@@ -1,20 +1,16 @@
 <?php
 namespace Classes;
 
-
 class Email {
     public $email;
     public $nombre;
     public $apellido;
     public $token;
-    public function __construct($email,$nombre,$apellido,$token)
-    {
+
+    public function __construct($email, $nombre, $apellido, $token) {
         $this->email = $email;
         $this->nombre = $nombre;
         $this->apellido = $apellido;
         $this->token = $token;
-      
     }
-
-
 }
