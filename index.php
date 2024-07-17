6efbb2a845e242649472d2ae1cbc0b7965f2a114
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-07-16 23:58:26 -0500

    Update index.php

--- index.php
+++ index.php
@@ -30,4 +30,4 @@ $router->post('/crear-cuenta', [LoginController::class, 'crear']);
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
-debuguear($router->rutasGET);
+// debuguear($router->rutasGET);
