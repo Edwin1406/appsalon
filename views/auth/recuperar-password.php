31d3afe34d35939eea2aba8e298393f43dbb7c05
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-07-18 15:25:06 -0500

    Update recuperar-password.php

--- views/auth/recuperar-password.php
+++ views/auth/recuperar-password.php
@@ -1,1 +1,26 @@
 <h1 class="nombre-pagina"> Recuperar Password</h1>
+<p class="descripcion-pagina">Coloca tu nueva contraseña</p>
+
+<form class="formulario" method="POST">
+    <?php include_once __DIR__. '/../templates/alertas.php'; ?> 
+    <div class="campo">
+        <label for="password">Password</label>
+        <input 
+            type="password" 
+            id="password" 
+            placeholder="Tu Password" 
+            name="password"
+            />
+    </div>
+    <div class="campo">
+        <label for="confirmar">Confirmar Password</label>
+        <input 
+            type="password" 
+            id="confirmar" 
+            placeholder="Repite tu Password" 
+            name="confirmar"
+            />
+    </div>
+    <input type="submit" class="boton boton-verde" value="Cambiar Password">
+
+</form>
