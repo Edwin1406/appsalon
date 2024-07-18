29f1144c7dae2f342bd1354c54acde686d2a7ad2
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-07-18 10:30:18 -0500

    Update confirmar.php

--- views/auth/confirmar.php
+++ views/auth/confirmar.php
@@ -1,6 +1,5 @@
 <h1 class="nombre-pagina">confirmar cuenta</h1>
 <?php include_once __DIR__. '/../templates/alertas.php'; ?> 
 <div class="acciones">
-    <a href="/crear-cuenta">¿No tienes cuenta? Regístrate</a>
-    <a href="/olvide">¿Olvidaste tu contraseña?</a>
+    <a href="/">Iniciar Sesión</a>
 </div>
