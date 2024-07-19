fef0a49a3a2645a2bca0a1cb2886936e46fcbc9d
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-07-18 21:37:47 -0500

    Update index.php

--- views/cita/index.php
+++ views/cita/index.php
@@ -1,2 +1,18 @@
 <h1 class="nombre-pagina">Crear nueva cita</h1>
-<p class="descripcion-pagina">Elige tus servicios a continuación</p>
+<p class="descripcion-pagina">Elige tus servicios y coloca tus datos</p>
+<div id="app">
+    <div id="paso-1">
+        <h2>servicios</h2>
+        <p>Elige tus servicios a continuación</p>
+        <div id="servicios" class="lista-servicios"></div>
+    </div>
+    <div id="paso-2">
+    <h2>Tus datos y cita</h2>
+    <p>Coloca tus datos y fecha de tu cita</p>
+
+    </div>
+    <div id="paso-3">
+
+    </div>
+
+</div>
