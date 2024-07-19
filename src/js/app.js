ae8002e43e3546427cebfbb89713c24077f119e3
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-07-19 09:45:38 -0500

    .

--- src/js/app.js
+++ src/js/app.js
@@ -6,12 +6,27 @@ document.addEventListener('DOMContentLoaded', function() {
 function inciarApp() { 
     tabs(); //cambiar la seccion cuando se da click en un tab
 }
+function mostrarSeccion(paso){
+    //ocultar la seccion anterior
+    const seccionAnterior = document.querySelector('.mostrar');
+    seccionAnterior.classList.remove('mostrar');
+  
+
+    // seleccionar la seccion con el paso
+    const pasoSelector= `#paso-${paso}`;
+    const seccion = document.querySelector(pasoSelector);
+    seccion.classList.add('mostrar');
+
+
+
+}
 
 function tabs(){
     const botones = document.querySelectorAll('.tabs button');
     botones.forEach(boton =>{
         boton.addEventListener('click', (e) =>{
-            console.log(e.target.dataset.paso);
+            paso = parseInt(e.target.dataset.paso);
+            mostrarSeccion(paso);
         });
     });
 
