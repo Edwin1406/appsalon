fd5c7847209c5a395edf54c3ee5fd6e94e6c1eb5
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-07-19 21:37:14 -0500

    .

--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -1,14 +1,17 @@
 <?php
 namespace Controllers;
 
+use Model\Servicio;
 use MVC\Router;
 
 class ApiController {
     public static function index(Router $router) {
-        
+
+        $servicios = Servicio::all();
         $router->render('api/index',[
-            
+            'servicios' => $servicios
         ]);
+        echo json_encode($servicios);
     }
 }
 
