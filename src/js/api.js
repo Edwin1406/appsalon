06ba3b47d30a2861e66685677b2d10ff8acc069a
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-07-19 22:21:02 -0500

    .

--- src/js/api.js
+++ src/js/api.js
@@ -4,20 +4,28 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 function inciarApp() {
-    mostrarServicios();
+    ApiServicios(); //consultando la api de servicios
 }
 
 
 
-async function mostrarServicios(){
+async function ApiServicios(){
     try {
         const url = 'https://serviacrilico.com/api/servicios';
         const resultado = await fetch(url);
-        const json = await resultado.json();
-        console.log(json);
+        const servicios = await resultado.json();
+        mostrarServicios(servicios);
     } catch (e) {
       console.log(e);
         
     }
+
+}
+
+function mostrarServicios(servicios){
+    servicios.forEach(servicio => {
+        const {id,nombre,precio} =servicio;
+        console.log(id,nombre,precio);
+    });
 
 }
