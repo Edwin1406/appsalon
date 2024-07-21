46d36e9546f9856dc6982c0968b8fbae98d4bcb9
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-07-20 20:43:43 -0500

    Update mensaje.php

--- views/auth/mensaje.php
+++ views/auth/mensaje.php
@@ -1,2 +1,5 @@
 <h1 class="nombre-pagina">Confirma tu cuenta</h1>
 <p class="descripcion-pagina">Hemos enviado las intrucciones para confirma tu cuenta a tu email.</p>
+<div class="acciones">
+    <a href="/">Inicia Sessión</a>
+</div>
