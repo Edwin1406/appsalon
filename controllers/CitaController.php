210ec5e27075350f58c9f5bdb644abe318b74e96
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-07-22 17:13:12 -0500

    Update CitaController.php

--- controllers/CitaController.php
+++ controllers/CitaController.php
@@ -7,10 +7,10 @@ class CitaController {
     public static function index( Router $router ) {
         session_start();
     // Imprimir los valores de la sesión
-        $id= $_SESSION['id'];
-    // debuguear($_SESSION['id']);
+    
+    debuguear($_SESSION['id']);
         $router->render('cita/index',[
-            'id' => $id,
+            'id' => $_SESSION['id'],
             'nombre' => $_SESSION['nombre'],
         ]);
     }
