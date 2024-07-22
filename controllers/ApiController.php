52239583aa685b73763de409259577e7c10e6b59
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-07-22 16:58:05 -0500

    .

--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -1,6 +1,7 @@
 <?php
 namespace Controllers;
 
+use Model\Cita;
 use Model\Servicio;
 
 
@@ -13,11 +14,15 @@ class ApiController {
     }
 
     public static function guardar(){
-        $respuesta = [
-            'datos' => $_POST,
-
-        ]; // Arreglo para la respuesta asocitiva
+    
+        $cita= new Cita($_POST);
+        $respuesta = $cita->guardar();
         echo json_encode($respuesta);
+        //     $respuesta = [
+        //     'cita' => $cita
+
+        // ]; // Arreglo para la respuesta asocitiva
+
     }
 
 
