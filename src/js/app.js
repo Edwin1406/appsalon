56ec46bc4a05218cc99d18a0b08886a08da61c8d
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-07-22 18:56:53 -0500

    .

--- src/js/app.js
+++ src/js/app.js
@@ -184,6 +184,7 @@ function seleccionarServicio (servicio){
 function nombreCita(){
     const nombre=document.querySelector('#nombre').value;
     cita.nombre = nombre;
+    cita.id=id;
     console.log(cita);
 }
 
