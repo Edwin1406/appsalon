e3a0c13fc9dd7f1a0500c0aaca5125e933840fe1
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-07-22 18:52:55 -0500

    .

--- controllers/CitaController.php
+++ controllers/CitaController.php
@@ -6,14 +6,10 @@ use MVC\Router;
 class CitaController {
     public static function index( Router $router ) {
         session_start();
-    // Imprimir los valores de la sesión
-    //  debuguear($_SESSION);
-    
-          
-
+   
         $router->render('cita/index',[
             'nombre' => $_SESSION['nombre'],
-            'id' => $_SESSION['id'],
+            'id' => $_SESSION['id']
         ]);
     }
 }
