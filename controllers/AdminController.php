238de84eca7ccfffbe29dc4c4530c36cdd64d8a9
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-07-23 21:08:43 -0500

    Update AdminController.php

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -7,6 +7,7 @@ class AdminController
 {
     public static function index(Router $router)
     {
+        session_start();
      
       $router->render('admin/index',[
         'nombre' => $_SESSION['nombre']
