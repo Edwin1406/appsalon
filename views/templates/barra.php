aeeb49cb0c54f162f7baf8da933092bed03bf3de
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-07-26 18:52:13 -0500

    Update barra.php

--- views/templates/barra.php
+++ views/templates/barra.php
@@ -2,3 +2,11 @@
     <p>Bienvenido: <?php echo $nombre ?? ''; ?></p>
     <a href="/logout" class="boton">Cerrar Session</a>
 </div>
+
+<?php
+if(isset($_SESSION['admin'])){
+    echo ' es admin';
+
+}else{
+    echo 'no es admin';
+}
