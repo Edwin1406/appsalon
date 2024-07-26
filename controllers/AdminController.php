b016952cea2c95fddb358a684b87465cca3b873a
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-07-26 18:23:22 -0500

    Update AdminController.php

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -29,7 +29,9 @@ class AdminController
         $consulta .= " ON citasservicios.citaId=citas.id ";
         $consulta .= " LEFT OUTER JOIN servicios ";
         $consulta .= " ON servicios.id=citasservicios.servicioId ";
-        $consulta .= " WHERE fecha =  '${fecha}' ";
+        // $consulta .= " WHERE fecha =  '${fecha}' ";
+        $consulta .= " WHERE fecha = '{$fecha}' ";
+
         $citas=AdminCita::SQL($consulta);
         
         // debuguear($citas);     
