f9c9f97ba5a839ba1ca278c872ae9578d0e69277
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-07-26 18:57:32 -0500

    Update barra.php

--- views/templates/barra.php
+++ views/templates/barra.php
@@ -3,10 +3,11 @@
     <a href="/logout" class="boton">Cerrar Session</a>
 </div>
 
-<?php
-if(isset($_SESSION['admin'])){
-    echo ' es admin';
+<?php if(isset($_SESSION['admin'])): ?>
+    <div class="barra-servicios">
+        <a class="boton" href="/admin">Ver Citas</a>
+        <a class="boton" href="/servicios">Ver Servicios</a>
+        <a class="boton" href="/servicios/crear">Nuevo Servicio</a>
 
-}else{
-    echo 'no es admin';
-}
+    </div>
+<?php endif; ?>
