219a4b12aa7044166e9bfc96b8e86d93f86c4a3b
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-07-26 19:46:00 -0500

    Update formulario.php

--- views/servicios/formulario.php
+++ views/servicios/formulario.php
@@ -1,5 +1,5 @@
 <div class="campo">
     <label for="nombre">Nombre:</label>
-    <input type="text" name="nombre" id="nombre" value="<?php echo $servicio->nombre; ?>">
-    
+    <input type="text" name="nombre" id="nombre" value="nombre">
+
 </div>
