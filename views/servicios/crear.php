b2a98003fbf76974343156e55014d2f321d80aaf
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-07-26 20:53:32 -0500

    Update crear.php

--- views/servicios/crear.php
+++ views/servicios/crear.php
@@ -1,7 +1,7 @@
 <h1 class="nombre-pagina">Nuevo Servicios</h1>
 <p class="descripcion-pagina">Llenar todos los campos para añadir un nuevo Servicio</p>
 <?php 
-include_once __DIR__. '/../templates/barra.php';
+// include_once __DIR__. '/../templates/barra.php';
 include_once __DIR__. '/../templates/alertas.php';
 
 ?>
