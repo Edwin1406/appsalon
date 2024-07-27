01f7cfd21ad3896d23f062a0047deec04f210b10
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-07-27 10:37:06 -0500

    .

--- src/js/app.js
+++ src/js/app.js
@@ -264,8 +264,8 @@ function mostrarHoras(horas) {
         const horaCita = e.target.value
         const hora = horaCita.split(":");
         const horaReservada = horasReservadas.includes(horaCita);
-        // const fechaReservada = fechaReservada.includes(fechaReservada);
-        // console.log(`fecha reservada: ${fechaReservada}`);
+        const fechaReservada = fechaReservada.includes(fechacorta);
+        console.log(`fecha reservada: ${fechaReservada}`);
         console.log(`hora reservada: ${horaReservada}`);
     
         if(hora[0] < 10 || hora[0] > 18){
