9184a5f9c994c072a89edec7fcbe9aa2be884f43
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-07-26 19:25:30 -0500

    Update ServicioController.php

--- controllers/ServicioController.php
+++ controllers/ServicioController.php
@@ -1,11 +1,17 @@
 <?php
 namespace Controllers;
 
+use Model\Servicio;
 use MVC\Router;
 
 class ServicioController{
     public static function index(Router $router){
-        $router->render('servicios/index');
+        $servicios = Servicio::all();
+        debuguear($servicios);
+        $router->render('servicios/index',[
+          'servicios' => $servicios
+
+        ]);
         
     }
     public static function crear(Router $router){
