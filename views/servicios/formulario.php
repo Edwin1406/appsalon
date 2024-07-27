0493648b0f261d4e0dfeda7ac5ccc58cafe0d6c6
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-07-26 20:33:02 -0500

    Update formulario.php

--- views/servicios/formulario.php
+++ views/servicios/formulario.php
@@ -1,7 +1,13 @@
 <div class="campo">
     <label for="nombre">Nombre:</label>
-    <input type="text" name="nombre" id="nombre" value="nombre">
+    <input type="text"
+     name="nombre" 
+     id="nombre"
+     placeholder="Nombre del Servicio">
     <label for="precio">Precio</label>
-    <input type="number" name="precio" id="precio" value="precio">
+    <input type="number" 
+    name="precio" 
+    id="precio" 
+    placeholder="Precio">
 
 </div>
