916e4ec9244577afee2213710872b61154615547
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-07-26 23:10:50 -0500

    .

--- src/js/app.js
+++ src/js/app.js
@@ -264,15 +264,15 @@ function seleccionarHora(horasReservadas) {
     const inputHora = document.querySelector('#hora');
     inputHora.addEventListener('input', function(e) {
         const horaCita = e.target.value;
-        const hora = horaCita.split(":");
+        const [hora, minuto, segundo] = horaCita.split(":");
+        const horaFormateada = `${hora}:${minuto}`;  // Formato HH:MM
 
-        // Convertir la hora a un formato que facilite la comparación
-        const horaCompleta = parseInt(hora[0], 10);
-
+        // Validar el rango de hora
+        const horaCompleta = parseInt(hora, 10);
         if (horaCompleta < 10 || horaCompleta > 18) {
             e.target.value = '';
-            mostrarAlerta('Hora no valida', 'error', '.formulario');
-        } else if (horasReservadas.includes(horaCita)) {
+            mostrarAlerta('Hora no válida', 'error', '.formulario');
+        } else if (horasReservadas.includes(horaFormateada)) {
             e.target.value = '';
             mostrarAlerta('Hora ya reservada', 'error', '.formulario');
         } else {
