c2085a45d4c9d275765c4f1ea1ea6eb7334b272b
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-07-26 20:50:54 -0500

    Update ServicioController.php

--- controllers/ServicioController.php
+++ controllers/ServicioController.php
@@ -20,14 +20,19 @@ class ServicioController{
     }
     public static function crear(Router $router){
         session_start();
+        // crear un nuevo servicio vacio
         $servicio = new Servicio;
         if($_SERVER['REQUEST_METHOD'] === 'POST'){
+            // este metodo se encarga de llenar el objeto servicio con los datos enviados por el usuario
             $servicio->sincronizar($_POST);
+            // validacion
+            $alerta = [];
 
         }
         $router->render('servicios/crear',[
             'nombre' => $_SESSION['nombre'],
-            'servicio' => $servicio
+            'servicio' => $servicio,
+            'alerta' => $alerta
         ]);
     }
     public static function actualizar(Router $router){
