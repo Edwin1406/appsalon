b6723e7e691391cc38d29100111e6d0e59493dca
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-07-26 21:03:31 -0500

    Update alertas.php

--- views/templates/alertas.php
+++ views/templates/alertas.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
 
 foreach( $alertas as $key => $mensajes):
     foreach($mensajes as $mensaje):
@@ -12,4 +12,16 @@ foreach( $alertas as $key => $mensajes):
     endforeach;
 endforeach;
 
+?> -->
+
+<?php
+// Asegúrate de que $alertas sea un array antes de usar foreach
+if (is_array($alertas)) {
+    foreach ($alertas as $key => $mensaje) {
+        echo "<div class='alerta $key'>$mensaje</div>";
+    }
+} else {
+    // Maneja el caso en que $alertas no sea un array
+    echo "<div class='alerta error'>No hay alertas para mostrar</div>";
+}
 ?>
