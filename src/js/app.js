d264d734d6526369c7a1b9e31e99b6ba942f196b
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-07-26 23:52:52 -0500

    .

--- src/js/app.js
+++ src/js/app.js
@@ -243,21 +243,12 @@ function mostrarHoras(horas) {
     
 }
 
-function seleccionarHoras(){
-    const selectHoras = document.querySelector('#hora');
-    let hora = 10;
-    for(let i = 0; i < 8; i++){
-        const option = document.createElement('OPTION');
-        option.value = `${hora < 10 ? `0${hora}`:hora}:00`;
-        option.textContent = `${hora < 10 ? `0${hora}`:hora}:00`;
-        selectHoras.appendChild(option);
-        hora++;
-    }
+function seleccionarHoras(horasReservadas){
+    console.log(horasReservadas);
 }
 
 
-function seleccionarHora(horasReservadas){
-
+function seleccionarHora(){
 
     const inputHora = document.querySelector('#hora');
     inputHora.addEventListener('input',function(e){
