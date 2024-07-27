83405547384ab2bcdf5a8b2c00a888639989c732
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-07-26 19:44:52 -0500

    Update crear.php

--- views/servicios/crear.php
+++ views/servicios/crear.php
@@ -1,3 +1,9 @@
 <h1 class="nombre-pagina">Nuevo Servicios</h1>
 <p class="descripcion-pagina">Llenar todos los campos para añadir un nuevo Servicio</p>
 <?php include_once __DIR__. '/../templates/barra.php' ?>
+<form action="/servicios/crear" method="POST" class="formulario">
+    <?php include_once __DIR__. '/formulario.php' ?>
+
+    <input type="submit" value="Crear Servicio" class="boton boton-verde">
+
+</form>
