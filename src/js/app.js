2d39c2ff490c73a7841f09d3e33cc8175156424a
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-07-27 22:44:39 -0500

    .

--- src/js/app.js
+++ src/js/app.js
@@ -294,29 +294,29 @@ function mostrarHoras(horas) {
     });
 }
 
-function seleccionarHora(){
+// function seleccionarHora(){
 
-    const inputHora = document.querySelector('#hora');
-    inputHora.addEventListener('input',function(e){
+//     const inputHora = document.querySelector('#hora');
+//     inputHora.addEventListener('input',function(e){
     
-    const horaCita = e.target.value
-    const hora = horaCita.split(":");
+//     const horaCita = e.target.value
+//     const hora = horaCita.split(":");
 
-    if(hora[0] < 10 || hora[0] >= 18){
-        e.target.value = '';
-    //    mostrarAlerta('Hora no valida','error','.formulario');
-    Swal.fire({
-        icon: "error",
-        title: "Hora no valida",
-        text: "Por favor selecciona una hora entre las 10:00 y las 18:00",
-      });
-    }else{
-        cita.hora = horaCita;
-        console.log(cita);
+//     if(hora[0] < 10 || hora[0] >= 18){
+//         e.target.value = '';
+//     //    mostrarAlerta('Hora no valida','error','.formulario');
+//     Swal.fire({
+//         icon: "error",
+//         title: "Hora no valida",
+//         text: "Por favor selecciona una hora entre las 10:00 y las 18:00",
+//       });
+//     }else{
+//         cita.hora = horaCita;
+//         console.log(cita);
       
-    }
-    })
-}
+//     }
+//     })
+// }
 
 
 
