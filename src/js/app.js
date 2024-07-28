ab51adfc77627f087a68625105068fdaa021396c
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-07-27 22:29:46 -0500

    .

--- src/js/app.js
+++ src/js/app.js
@@ -293,7 +293,12 @@ function seleccionarHora(){
 
     if(hora[0] < 10 || hora[0] > 18){
         e.target.value = '';
-       mostrarAlerta('Hora no valida','error','.formulario');
+    //    mostrarAlerta('Hora no valida','error','.formulario');
+    Swal.fire({
+        icon: "error",
+        title: "Hora no valida",
+        text: "Por favor selecciona una hora entre las 10:00 y las 18:00",
+      });
     }else{
         cita.hora = horaCita;
         console.log(cita);
