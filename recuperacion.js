84156282852828837c5b5dbf94661a566677c6e1
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-07-27 23:03:30 -0500

    .

---
+++ recuperacion.js
@@ -0,0 +1,24 @@
+
+// function seleccionarHora(){
+
+//     const inputHora = document.querySelector('#hora');
+//     inputHora.addEventListener('input',function(e){
+    
+//     const horaCita = e.target.value
+//     const hora = horaCita.split(":");
+
+//     if(hora[0] < 10 || hora[0] >= 18){
+//         e.target.value = '';
+//     //    mostrarAlerta('Hora no valida','error','.formulario');
+//     Swal.fire({
+//         icon: "error",
+//         title: "Hora no valida",
+//         text: "Por favor selecciona una hora entre las 10:00 y las 18:00",
+//       });
+//     }else{
+//         cita.hora = horaCita;
+//         console.log(cita);
+      
+//     }
+//     })
+// }
