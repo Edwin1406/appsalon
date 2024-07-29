af15e7d96d21a44f5b4d7065c3e18ed6a345d11d
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-07-28 20:12:20 -0500

    Update index.php

--- views/servicios/index.php
+++ views/servicios/index.php
@@ -7,6 +7,10 @@
         <li>
             <p> Nombre : <span> <?php echo $servicio->nombre?></span></p>
             <p> Precio : <span> $ <?php echo $servicio->precio?></span></p>
+            <div class="acciones">
+                <a href="/servicios/actualizar?id=<?php echo $servicio->id ?>">Actualizar</a>
+            </div>
         </li>
     <?php endforeach?>
+
 </ul>
