431c0d0c2d0f382c318e2488860834702cc98b06
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-07-28 20:23:05 -0500

    Update actualizar.php

--- views/servicios/actualizar.php
+++ views/servicios/actualizar.php
@@ -7,9 +7,9 @@ include_once __DIR__. '/../templates/alertas.php';
 ?>
 
 
-<form action="/servicios/crear" method="POST" class="formulario">
+<form  method="POST" class="formulario">
     <?php include_once __DIR__. '/formulario.php' ?>
 
-    <input type="submit" value="Crear Servicio" class="boton boton-verde">
+    <input type="submit" value="Actualizar Servicio" class="boton boton-verde">
 
 </form>
