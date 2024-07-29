f22bc06750a9d3e904c589442481b72dccc1537a
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-07-28 21:18:25 -0500

    Update app.php

--- includes/app.php
+++ includes/app.php
@@ -1,10 +1,11 @@
 <?php 
 use Model\ActiveRecord;
 require_once __DIR__ . '/../vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
 
 require 'funciones.php';
 require 'database.php';
+
 // Conectarnos a la base de datos
 ActiveRecord::setDB($db);
