eae017d0cb1eb261ba56c1415bb3ed0818e0210b
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-07-30 10:16:23 -0500

    Update inicio.php

--- views/auth/inicio.php
+++ views/auth/inicio.php
@@ -1,9 +1,10 @@
     <link rel="stylesheet" href="/public/build/css/style.css">
 
 <header class="header">
+
     <div class="contenedor">
         <div class="barra">
-            <a class="logo" href="/login">
+            <a class="logo" href="/">
             <h1>Blog <span>Odontologia </span></h1>
             </a>
          
@@ -13,5 +14,6 @@
         <h2 class="no-margin">hola</h2>
     <p class="no-margin">aprtende de mi </p>
     </div>
+
 
 </header>
