146a4acde816923a21ef46e4b4f870cfad020c78
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-07-29 21:24:53 -0500

    .

---
+++ views/layoutinicio.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="Aplicación de citas odontologicas">
+    <meta name="keywords" content="citas, odontologicas, dentista">
+    <meta name="author" content="Edwin Diaz">
+    <link rel="shortcut icon" href="public/build/img/dentista.jpg" type="image/x-icon">
+    <title>Home</title>
+
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;700;900&display=swap" rel="stylesheet"> 
+    <link rel="stylesheet" href="/public/build/css/app.css">
+    
+</head>
+<body>
+<header class="header">
+<h1>dddd</h1>
+</header>
+    <div class="contenedor">
+        
+            <?php echo $contenido; ?>
+        
+    </div>
+    <?php echo $script ?? ''; ?>
+
+           
+</body>
+</html>
