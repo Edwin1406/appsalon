9a681a10fb16f2de4c74e8ac636cb4f4884be958
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-07-30 10:08:00 -0500

    Update inicio.php

--- views/auth/inicio.php
+++ views/auth/inicio.php
@@ -1,6 +1,9 @@
     <link rel="stylesheet" href="/public/build/css/style.css">
 
 <header class="header">
-    
+    <div class="heade__texto">
+        <h2 class="no-margin">hola</h2>
+    <p class="no-margin">aprtende de mi </p>
+    </div>
 
 </header>
