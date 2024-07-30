faf5c590fa0a681d270f4a999ee12f3af7e2bec4
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-07-30 09:48:49 -0500

    .

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,3 +1,5 @@
+<link rel="stylesheet" href="/public/build/css/app.css">
+
 <h1 class="nombre-pagina">Login</h1>
 <p class="descripcion-pagina">Inicia Sesión con tus datos</p>
 <?php include_once __DIR__. '/../templates/alertas.php'; ?> 
