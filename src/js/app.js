4c088723ff15d57009867b5dff4318c82c74b259
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-08-12 13:15:43 -0500

    .

--- src/js/app.js
+++ src/js/app.js
@@ -134,6 +134,7 @@ async function ApiServicios(){
 function mostrarServicios(servicios){
     servicios.forEach(servicio => {
         const {id,nombre,precio,odontologId} =servicio;
+        console.log(servicio);
         // scripting de los servicios
         const nombreServicio = document.createElement('P');
         nombreServicio.classList.add('nombre-servicio');
