d6c7c19b9c5621afefab3ebe8bbbf0846453e2bb
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-08-12 20:44:48 -0500

    Update index.php

--- views/admin/index.php
+++ views/admin/index.php
@@ -76,15 +76,24 @@
     // Establecer la zona horaria de Ecuador
     date_default_timezone_set('America/Guayaquil');
 
-    // Configurar localización en español para la fecha
-    setlocale(LC_TIME, 'es_ES.UTF-8', 'es_ES', 'Spanish_Spain', 'Spanish');
-    
+    // Crear un objeto DateTime para la fecha de la cita
+    $date = new DateTime($cita->fechas_whats);
+
+    // Formatear la fecha como "martes 13 de agosto del 2024"
+    $formatter = new IntlDateFormatter(
+        'es_ES', 
+        IntlDateFormatter::FULL, 
+        IntlDateFormatter::NONE,
+        'America/Guayaquil',
+        IntlDateFormatter::GREGORIAN
+    );
+    $appointment_date = $formatter->format($date);
+
     // Eliminar el cero del teléfono
     $telefono_sin_cero = ltrim($cita->telefono, '0');
     $business_name = "NEW DENTAL";
     $client_name = $cita->cliente; // Asume que $cita->nombre contiene el nombre del cliente
-    $appointment_date = strftime('%A %d de %B del %Y', strtotime($cita->fechas_whats)); // Formatear la fecha como "martes 13 de agosto del 2024"
-    $appointment_time = date('H:i', strtotime($cita->hora)); // Asume que $cita->hora es la hora de la cita
+    $appointment_time = $date->format('H:i'); // Formatear la hora de la cita
     $phone_number = $telefono_sin_cero; // Número de WhatsApp incluyendo el código del país (ej. 593 para Ecuador)
 
     // Obtener la hora actual del servidor en la zona horaria de Ecuador
