6f37763aebfdf52468170fb18e4c19920d10ebe5
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-08-15 20:43:12 -0500

    Update ApiHoraController.php

--- controllers/ApiHoraController.php
+++ controllers/ApiHoraController.php
@@ -5,6 +5,7 @@ use Model\Cita;
 
 class ApiHoraController{
     public static function index() {
+        header("Access-Control-Allow-Origin: *");
 
         $cita = Cita::all();
         echo json_encode($cita);
