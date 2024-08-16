905aa5df2fe18168170afd9b12db98802bb7fc2d
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-08-15 20:46:53 -0500

    Update ApiHoraController.php

--- controllers/ApiHoraController.php
+++ controllers/ApiHoraController.php
@@ -5,7 +5,19 @@ use Model\Cita;
 
 class ApiHoraController{
     public static function index() {
+        
+        // Permitir acceso desde cualquier origen
         header("Access-Control-Allow-Origin: *");
+        
+        // Permitir métodos específicos (GET, POST, PUT, DELETE, OPTIONS)
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+        
+        // Permitir headers específicos (Content-Type, Authorization, etc.)
+        header("Access-Control-Allow-Headers: Content-Type, Authorization");
+        
+        // Permitir credenciales (cookies, autenticación HTTP, etc.)
+        // header("Access-Control-Allow-Credentials: true");
+        
 
         $cita = Cita::all();
         echo json_encode($cita);
