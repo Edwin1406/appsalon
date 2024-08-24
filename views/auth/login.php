743a2165259a496c9656ca3eba0cff609b5866e8
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-08-23 23:09:45 -0500

    Update login.php

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,31 +1,31 @@
-<div class="contenedor login">
-    <div class="contenedor-sm">
-    <h2 class="auth__heading"><?php echo $titulo ?? '' ?></h2>
-    <p class="auth__texto">Inicia Sesion en Sitio Web </p>
-        <?php include_once __DIR__ . '/../templates/alertas.php' ?>
+<link rel="stylesheet" href="/public/build/css/app.css">
 
-        <form class="formulario"  method="POST">
-            <div class="campo">
-                <label for="email">Email</label>
-                <input 
-                    type="email" 
-                    name="email" 
-                    id="email" 
-                    placeholder="Tu Email">
-            </div>
-            <div class="campo">
-                <label for="password">Password</label>
-                <input 
-                    type="password" 
-                    name="password" 
-                    id="password" 
-                    placeholder="Tu Password">
-            </div>
-            <input type="submit" class="boton boton-verde" value="Iniciar Sesión">
+<h1 class="nombre-pagina">Login</h1>
+<p class="descripcion-pagina">Inicia Sesión con tus datos</p>
+<?php include_once __DIR__. '/../templates/alertas.php'; ?> 
 
-        </form>
+<form class="formulario" method="POST" action="/">
+    <div class="campo">
+        <label for="email">Email</label>
+        <input 
+            type="email" 
+            id="email" 
+            placeholder="Tu Email" 
+            name="email"
+        />
+    </div>
+    <div class="campo">
+        <label for="password">Password</label>
+        <input 
+            type="password" 
+            id="password" 
+            placeholder="Tu Password" 
+            name="password"
+            />
+    </div>
+    <input type="submit" class="boton boton-verde" value="Iniciar Sesión">
+</form>
 <div class="acciones">
     <a href="/crear-cuenta">¿No tienes cuenta? Regístrate</a>
     <a href="/olvide">¿Olvidaste tu contraseña?</a>
-</div>
 </div>
