81ed8b3c44e5410d19f2a64f5e77a68314dd19a2
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-10-29 13:14:06 -0500

    Update index.php

--- index.php
+++ index.php
@@ -23,21 +23,21 @@ $router->post('/admin/login', [LoginController::class, 'login']);
 $router->get('/admin/logout', [LoginController::class, 'logout']);
 
 // Recuperar contraseña
-$router->get('/olvide', [LoginController::class, 'olvide']);
-$router->post('/olvide', [LoginController::class, 'olvide']);
-$router->get('/recuperar', [LoginController::class, 'recuperar']);
-$router->post('/recuperar', [LoginController::class, 'recuperar']);
+$router->get('/admin/olvide', [LoginController::class, 'olvide']);
+$router->post('/admin/olvide', [LoginController::class, 'olvide']);
+$router->get('/admin/recuperar', [LoginController::class, 'recuperar']);
+$router->post('/admin/recuperar', [LoginController::class, 'recuperar']);
 
 // Rutas Crear Cuenta
-$router->get('/crear-cuenta', [LoginController::class, 'crear']);
-$router->post('/crear-cuenta', [LoginController::class, 'crear']);
+$router->get('/admin/crear-cuenta', [LoginController::class, 'crear']);
+$router->post('/admin/crear-cuenta', [LoginController::class, 'crear']);
 
 // Confirmar cuenta
-$router->get('/confirmar', [LoginController::class, 'confirmar']);
-$router->get('/mensaje', [LoginController::class, 'mensaje']);
+$router->get('/admin/confirmar', [LoginController::class, 'confirmar']);
+$router->get('/admin/mensaje', [LoginController::class, 'mensaje']);
 
 // Rutas de administrador
-$router->get('/cita', [CitaController::class, 'index']);
+$router->get('/admin/cita', [CitaController::class, 'index']);
 $router->get('/admin', [AdminController::class, 'index']);
 
 
