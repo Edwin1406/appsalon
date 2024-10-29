48306d0b0d11f421f10ac823dd011412c5ac4e8f
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-10-29 13:28:47 -0500

    Update CitaController.php

--- controllers/CitaController.php
+++ controllers/CitaController.php
@@ -5,7 +5,7 @@ use MVC\Router;
 
 class CitaController {
     public static function index( Router $router ) {
-        // session_start();
+        session_start();
         // isAuth(); // Si no está autenticado lo redirige al login
         // debuguear($_SESSION['id']);
         $router->render('cita/index',[
