ea30c585e03c70550bf4fd4c6737059a5a411126
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-10-29 13:24:26 -0500

    .

--- src/js/app.js
+++ src/js/app.js
@@ -446,7 +446,7 @@ async function reservarCita(){
     try {
 
          //conexion a la api 
-      const url = `${location.origin}/api/citas`;
+      const url = `${location.origin}/admin/api/citas`;
       const respuesta = await fetch(url,{
           method:'POST',
           body:datos
