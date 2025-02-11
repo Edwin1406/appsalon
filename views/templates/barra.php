db33eb1a3632941a784dbe1be9d81ac479cb9631
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-02-11 16:45:34 -0500

    Update barra.php

--- views/templates/barra.php
+++ views/templates/barra.php
@@ -2,14 +2,14 @@
     <p>Bienvenido: <?php echo $nombre ?? ''; ?></p>
     <a href="/logout" class="boton">Cerrar Session</a>
 </div>
-
-<?php if(isset($_SESSION['admin'])): ?>
+<?php if(!empty($_SESSION['admin']) && $_SESSION['admin'] === true): ?>
     
     <div class="barra-servicios">
         <a class="boton" href="/admin">Ver Citas</a>
         <a class="boton" href="/admin/servicios">Ver Servicios</a>
         <a class="boton" href="/admin/servicios/crear">Nuevo Servicio</a>
         <a class="boton" href="/admin/servicios/calendario"> Ver Calendario</a>
-
     </div>
+
 <?php endif; ?>
+
