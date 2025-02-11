6d30d80a5e19c6bbb363cbf2f07f865c5d9fac29
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-02-11 13:03:09 -0500

    Update barra.php

--- views/templates/barra.php
+++ views/templates/barra.php
@@ -8,6 +8,7 @@
         <a class="boton" href="/admin">Ver Citas</a>
         <a class="boton" href="/servicios">Ver Servicios</a>
         <a class="boton" href="/servicios/crear">Nuevo Servicio</a>
+        <a class="boton" href="/servicios/calendario">Calendario</a>
 
     </div>
 <?php endif; ?>
