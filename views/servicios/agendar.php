5cc81150d70a433da06a4e39182425798e532576
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-02-12 17:01:05 -0500

    Update agendar.php

--- views/servicios/agendar.php
+++ views/servicios/agendar.php
@@ -9,6 +9,16 @@ include_once __DIR__. '/../templates/alertas.php';
 
 ?>
 
+<style>
+ .campo{
+    margin-bottom: 1rem;
+}
+
+
+</style>
+
+
+
 
 <form action="/admin/servicios/cliente" method="POST" class="formulario">
             
@@ -60,38 +70,36 @@ include_once __DIR__. '/../templates/alertas.php';
             />
         </div>
 
-
-
         <div class="campo">
-    <label for="hora">Hora</label>
-    <select id="hora" name="hora">
-        <option value="10:00">10:00</option>
-        <option value="10:30">10:30</option>
-        <option value="11:00">11:00</option>
-        <option value="11:30">11:30</option>
-        <option value="12:00">12:00</option>
-        <option value="12:30">12:30</option>
-        <option value="13:00">13:00</option>
-        <option value="13:30">13:30</option>
-        <option value="14:00">14:00</option>
-        <option value="14:30">14:30</option>
-        <option value="15:00">15:00</option>
-        <option value="15:30">15:30</option>
-        <option value="16:00">16:00</option>
-        <option value="16:30">16:30</option>
-        <option value="17:00">17:00</option>
-        <option value="17:30">17:30</option>
-        <option value="18:00">18:00</option>
-        <option value="18:30">18:30</option>
-        <option value="19:00">19:00</option>
-        <option value="19:30">19:30</option>
-        <option value="20:00">20:00</option>
-        <option value="20:30">20:30</option>
-        <option value="21:00">21:00</option>
-        <option value="21:30">21:30</option>
-        <option value="22:00">22:00</option>
-    </select>
-</div>
+            <label for="hora">Hora</label>
+            <select id="hora" name="hora">
+                <option value="10:00">10:00</option>
+                <option value="10:30">10:30</option>
+                <option value="11:00">11:00</option>
+                <option value="11:30">11:30</option>
+                <option value="12:00">12:00</option>
+                <option value="12:30">12:30</option>
+                <option value="13:00">13:00</option>
+                <option value="13:30">13:30</option>
+                <option value="14:00">14:00</option>
+                <option value="14:30">14:30</option>
+                <option value="15:00">15:00</option>
+                <option value="15:30">15:30</option>
+                <option value="16:00">16:00</option>
+                <option value="16:30">16:30</option>
+                <option value="17:00">17:00</option>
+                <option value="17:30">17:30</option>
+                <option value="18:00">18:00</option>
+                <option value="18:30">18:30</option>
+                <option value="19:00">19:00</option>
+                <option value="19:30">19:30</option>
+                <option value="20:00">20:00</option>
+                <option value="20:30">20:30</option>
+                <option value="21:00">21:00</option>
+                <option value="21:30">21:30</option>
+                <option value="22:00">22:00</option>
+            </select>
+        </div>
 
 
 
