fce96e533cff57c4505bacd9c58d3640634432c8
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-02-12 16:27:34 -0500

    Update cliente.php

--- views/servicios/cliente.php
+++ views/servicios/cliente.php
@@ -3,7 +3,7 @@
 <link rel="stylesheet" href="/public/build/css/app.css">
 
 <h1 class="nombre-pagina">Nuevo Cliente</h1>
-< class="descripcion-pagina">Llenar todos los campos para añadir un nuevo Cliente</p>
+<p class="descripcion-pagina">Llenar todos los campos para añadir un nuevo Cliente</p>
 <?php 
 // include_once __DIR__. '/../templates/barra.php';
 include_once __DIR__. '/../templates/alertas.php';
