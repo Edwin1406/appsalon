203f835b8e18c430804e48c512c76cd0f79c150e
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-02-12 16:28:59 -0500

    Update cliente.php

--- views/servicios/cliente.php
+++ views/servicios/cliente.php
@@ -13,15 +13,47 @@ include_once __DIR__. '/../templates/alertas.php';
 <form action="/admin/servicios/crear" method="POST" class="formulario">
             
 
-        <div class="campo">
-            <label for="nombre">Nombre:</label>
-            <input type="text"
-            name="nombre" 
-            id="nombre"
-            placeholder="Nombre del Servicio"
-            value="<?php echo $servicio->nombre; ?>">
-        </div>
-
+div class="campo">
+        <label for="nombre">Nombre</label>
+        <input 
+            type="text" 
+            id="nombre" 
+            placeholder="Tu Nombre" 
+            name="nombre"
+            value="<?php echo s($usuario->nombre); ?>"
+        />
+    </div>
+    <div class="campo">
+        <label for="apellido">Apellido</label>
+        <input 
+            type="text" 
+            id="apellido" 
+            placeholder="Tu Apellido" 
+            name="apellido"
+            value="<?php echo s($usuario->apellido); ?>"
+        />
+    </div>
+    <div class="campo">
+        <label for="telefono">Telefono</label>
+        <input 
+            type="tel" 
+            id="telefono" 
+            placeholder="Tu Telefono" 
+            name="telefono"
+            value="<?php echo s($usuario->telefono); ?>"
+        />
+    </div>
+    <div class="campo">
+        <label for="email">Email</label>
+        <input 
+            type="email" 
+            id="email" 
+            placeholder="Tu Email" 
+            name="email"
+            value="<?php echo s($usuario->email); ?>"
+        />
+    </div>
+   
     <input type="submit" value="Crear Servicio" class="boton boton-verde">
 
 </form>
