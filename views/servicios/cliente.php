2e9f344a685c4aaafc1b8d3024c558c45652492c
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-02-12 16:23:00 -0500

    Update cliente.php

--- views/servicios/cliente.php
+++ views/servicios/cliente.php
@@ -1,1 +1,32 @@
 <h1 class="nombre-pagina">Cliente</h1>
+
+<link rel="stylesheet" href="/public/build/css/app.css">
+
+<h1 class="nombre-pagina">Nuevo Servicios</h1>
+<p class="descripcion-pagina">Llenar todos los campos para añadir un nuevo Servicio</p>
+<?php 
+// include_once __DIR__. '/../templates/barra.php';
+include_once __DIR__. '/../templates/alertas.php';
+
+?>
+
+
+<form action="/admin/servicios/crear" method="POST" class="formulario">
+            
+
+        <div class="campo">
+            <label for="nombre">Nombre:</label>
+            <input type="text"
+            name="nombre" 
+            id="nombre"
+            placeholder="Nombre del Servicio"
+            value="<?php echo $servicio->nombre; ?>">
+        </div>
+
+    <input type="submit" value="Crear Servicio" class="boton boton-verde">
+
+</form>
+
+
+
+
