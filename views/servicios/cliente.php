94dd63d08efcec4c9cfe42a3506c49ad96cf676e
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-02-12 16:31:58 -0500

    Update cliente.php

--- views/servicios/cliente.php
+++ views/servicios/cliente.php
@@ -10,7 +10,7 @@ include_once __DIR__. '/../templates/alertas.php';
 ?>
 
 
-<form action="/admin/servicios/crear" method="POST" class="formulario">
+<form action="/admin/servicios/cliente" method="POST" class="formulario">
             
 
 <div class="campo">
