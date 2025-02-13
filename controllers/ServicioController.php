18a43f35d836445cfc723c86124524da2e5cd0ef
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-02-13 12:18:01 -0500

    Update ServicioController.php

--- controllers/ServicioController.php
+++ controllers/ServicioController.php
@@ -159,7 +159,7 @@ class ServicioController{
         isAdmin();
         
         $alertas = [];
-        $usuarios = Usuario::allDesc('ASC');
+        $usuarios = Usuario::allDesc('DESC');
         debuguear($usuarios);
         $odontologos = Odontologo::all();
         $servicios = Servicio::all();
