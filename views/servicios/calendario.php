9ab6e6052b1d1c00622ba1f92c48ac1840d7f975
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-02-12 22:29:35 -0500

    Update calendario.php

--- views/servicios/calendario.php
+++ views/servicios/calendario.php
@@ -225,7 +225,7 @@ document.addEventListener('DOMContentLoaded', function() {
                     const telefono = info.event.extendedProps.telefono;
                     
                     whatsappButton.onclick = function() {
-                        const whatsappURL = `https://wa.me/${telefono}?text=${encodeURIComponent(mensaje)}`;
+                        const whatsappURL = `https://wa.me/+593${telefono}?text=${encodeURIComponent(mensaje)}`;
                         window.open(whatsappURL, '_blank');
                     };
                     
