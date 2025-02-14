89a20e4c8533cc78edc90ee76fd889224b7b92b1
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-02-14 14:28:12 -0500

    Update index.php

--- views/admin/index.php
+++ views/admin/index.php
@@ -51,7 +51,7 @@ if (count($citas) === 0) {
                                 echo ($cita->estado == 'PENDIENTE') ? 'orange' : 
                                     (($cita->estado == 'CONFIRMADO') ? 'green' : 
                                     (($cita->estado == 'CANCELADO') ? 'red' : ''));
-                            ?>;">
+                            ?>
                                 <?php echo $cita->estado; ?>
                 
                 
