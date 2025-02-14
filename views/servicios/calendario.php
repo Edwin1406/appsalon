f4b368779e656b52fd930d11862b0fd8b58af8f1
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-02-14 15:51:43 -0500

    Update calendario.php

--- views/servicios/calendario.php
+++ views/servicios/calendario.php
@@ -1,6 +1,6 @@
 <h1 class="nombre-pagina">Calendario</h1>
-<?include_once __DIR__ . '/templates/barra.php' ?>
-fsdfsdfsdfsdfds
+<?php include_once __DIR__. '/../templates/barra.php' ?>
+
 
 <div class="contenido_ir">
 
