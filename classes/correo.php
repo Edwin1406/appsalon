7cfcd2c065daa133a3a7e52168de04b7a04c3d7a
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-02-15 12:42:21 -0500

    Update correo.php

--- classes/correo.php
+++ classes/correo.php
@@ -64,6 +64,7 @@ class correo {
             // Configurar el correo
             $mail->setFrom('pruebas@odonto.megawebsistem.com', 'Odonto Seguridad');
             $mail->addAddress($this->email);
+            $mail->addReplyTo('pruebas@odonto.megawebsistem.com', 'Soporte Odonto');
     
             $mail->Subject = 'Notificación de Inicio de Sesión';
             $mail->isHTML(true);
