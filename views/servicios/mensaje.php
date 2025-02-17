c4f3797e28c3ac675b7e63f10760a40f5f3908c9
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-02-17 17:00:33 -0500

    Update mensaje.php

--- views/servicios/mensaje.php
+++ views/servicios/mensaje.php
@@ -1,3 +1,10 @@
 <h1> <?php echo $titulo ?> </h1>
 
 <p>Gracias por actualizar tu estado. Esto nos ayuda a brindarte un mejor servicio y gestionar tu cita de manera eficiente.</p>
+
+<style>
+    p{
+        text-align: center;
+        font-size: 1.5rem;
+    }
+</style>
