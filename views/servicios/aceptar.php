10a9fecdf9ef666511e14d47da21de6dc3cb58ef
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-02-17 16:42:03 -0500

    Update aceptar.php

--- views/servicios/aceptar.php
+++ views/servicios/aceptar.php
@@ -1,4 +1,4 @@
-<h1 class="nombre-pagina">Actualizar Servicios</h1>
+<h1 class="nombre-pagina">Actualizar el estado de la Cita</h1>
 <p class="descripcion-pagina">Administracion de Servicios</p>
 <?php 
 // include_once __DIR__. '/../templates/barra.php';
