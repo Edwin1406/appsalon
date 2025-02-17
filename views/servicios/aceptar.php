de213ae3106de4a8fea330fb8fc76341dccf8282
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-02-17 16:32:55 -0500

    Update aceptar.php

--- views/servicios/aceptar.php
+++ views/servicios/aceptar.php
@@ -1,8 +1,8 @@
 <div class="campo">
     <label for="estado">Estado:</label>
     <select name="estado" id="estado">
-        <option value="pendiente" <?php echo ($aceptar->estado == 'pendiente') ? 'selected' : ''; ?>>Pendiente</option>
-        <option value="aceptado" <?php echo ($aceptar->estado == 'aceptado') ? 'selected' : ''; ?>>Aceptado</option>
-        <option value="cancelado" <?php echo ($aceptar->estado == 'cancelado') ? 'selected' : ''; ?>>Cancelado</option>
+        <option value="pendiente" <?php echo ($aceptar->estado == 'PENDIENTE') ? 'selected' : ''; ?>>Pendiente</option>
+        <option value="aceptado" <?php echo ($aceptar->estado == 'CONFIRMADO') ? 'selected' : ''; ?>>Aceptado</option>
+        <option value="cancelado" <?php echo ($aceptar->estado == 'CANCELADO') ? 'selected' : ''; ?>>Cancelado</option>
     </select>
 </div>
