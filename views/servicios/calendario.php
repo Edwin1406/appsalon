c073a79006256b85e84b3d78d94e229599a31cdd
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-03-07 12:23:59 -0500

    Update calendario.php

--- views/servicios/calendario.php
+++ views/servicios/calendario.php
@@ -287,7 +287,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
                     const eventos = data.map(cita => ({
                         id: cita.cita_id,
-                        title: `${cita.hora}- ${cita.nombrecliente} ${cita.apellidocliente}  ${cita.nota}`,
+                        title: `${cita.hora}- ${cita.nota} ${cita.nombrecliente} ${cita.apellidocliente} `,
                         start: cita.fecha, 
                         extendedProps: {
                             hora: cita.hora,
