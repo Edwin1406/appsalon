b3a2641c01f7e3d7a311103190a1c45040090bf5
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-03-15 22:30:11 -0500

    Update historial.php

---
+++ views/servicios/historial.php
@@ -0,0 +1,9 @@
+<link rel="stylesheet" href="/public/build/css/app.css">
+
+<h1 class="nombre-pagina">Nuevo Servicios</h1>
+<p class="descripcion-pagina">Llenar todos los campos para añadir un nuevo Servicio</p>
+<?php 
+// include_once __DIR__. '/../templates/barra.php';
+include_once __DIR__. '/../templates/alertas.php';
+
+?>
