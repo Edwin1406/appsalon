d2205cfe48accf00252b0bcc7e5480a452e7ad6d
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-06-09 16:42:59 -0500

    Update cliente.php

--- views/servicios/cliente.php
+++ views/servicios/cliente.php
@@ -10,6 +10,27 @@ include_once __DIR__. '/../templates/alertas.php';
 ?>
 
 
+<style>
+    .dashboard__boton {
+    text-decoration: none;
+    color: white;
+    background-color: #333;
+    padding: 1rem;
+    border-radius: 1rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    gap: 0.5rem;
+    min-width: 150px; /* Ajustar el tamaño mínimo */
+    white-space: nowrap; /* Evitar saltos de línea en el texto */
+}
+
+.dashboard__boton i {
+    font-size: 1rem; /* Ajustar el tamaño del icono */
+}
+</style>
+
 <div class="contenido_ir">
 
     <div class="dashboard__contenedor-boton">
