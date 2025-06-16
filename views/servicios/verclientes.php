c6557ddec7f056eb6fd2427f6ed20d5b1f92c966
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-06-16 08:50:32 -0500

    Update verclientes.php

--- views/servicios/verclientes.php
+++ views/servicios/verclientes.php
@@ -1,6 +1,6 @@
 
-<h1 class="nombre-pagina">Servicios</h1>
-<p class="descripcion-pagina">Administracion de Servicios</p>
+<h1 class="nombre-pagina">Cliente</h1>
+<p class="descripcion-pagina">Administracion de clientes</p>
 <?php include_once __DIR__. '/../templates/barra.php' ?>
 
 <ul class="servicios">
